1cf2b55856d18370d6e86c015ccbff08980e6250
Aram Drevekenin <grimsniffer@gmail.com>
2016-08-13 15:37:51 +0300

    basic functionality

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 module.exports = ids
 
 function ids (arr, lookfor) {
-  // TBD
+  return arr.reduce((pairs, elem, index, arr) => {
+    if (elem === lookfor && (index === 0 || elem !== arr[index - 1])) {
+      pairs.push({start: 0, end: 0})
+    } else if (elem === lookfor && elem === arr[index - 1]) {
+      pairs[pairs.length - 1].end = index
+    }
+    return pairs
+  }, [])
 }
