c05400a9c6caadc163f6fea6be053cd6e3cfb6a6
Aram Drevekenin <grimsniffer@gmail.com>
2016-08-13 15:39:16 +0300

    multiple sequences

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = ids
 function ids (arr, lookfor) {
   return arr.reduce((pairs, elem, index, arr) => {
     if (elem === lookfor && (index === 0 || elem !== arr[index - 1])) {
-      pairs.push({start: 0, end: 0})
+      pairs.push({start: index, end: index})
     } else if (elem === lookfor && elem === arr[index - 1]) {
       pairs[pairs.length - 1].end = index
     }
