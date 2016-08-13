00c776cedc6f2f100da36c9c32eae0ebf7d6381f
Aram Drevekenin <grimsniffer@gmail.com>
2016-08-13 16:05:48 +0300

    made index clearer

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ module.exports = ids
 
 function ids (arr, lookfor) {
   if (!arr || !Array.isArray(arr)) throw new Error(`${arr} is not an array`)
-  if (lookfor !== 0 && !lookfor) throw new Error('no element to search provided')
+  if (typeof lookfor === 'undefined') throw new Error('no element to search provided')
   return arr.reduce((pairs, elem, index, arr) => {
-    if (elem === lookfor && (index === 0 || elem !== arr[index - 1])) {
-      pairs.push({start: index, end: index})
-    } else if (elem === lookfor && elem === arr[index - 1]) {
-      pairs[pairs.length - 1].end = index
+    if (elem === lookfor) {
+      if (index === 0 || elem !== arr[index - 1]) {
+        // start of a sequence
+        pairs.push({start: index, end: index})
+      } else {
+        // middle or end of a sequence
+        pairs[pairs.length - 1].end = index
+      }
     }
     return pairs
   }, [])
